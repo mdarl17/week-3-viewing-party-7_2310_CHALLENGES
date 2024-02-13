bbc0d813edb901e2139ae6931d9f76b1ca179805
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-13 15:45:26 -0700

    Add validations for password_digest and password_confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User <ApplicationRecord 
-  validates_presence_of :email, :name 
+  validates_presence_of :email, :name, :password_digest, :password_confirmation
   validates_uniqueness_of :email
   has_many :viewing_parties
 end 
