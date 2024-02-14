2e030fc6a996bbaf7390efb1fd988f8c8688f6bd
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-14 15:31:18 -0700

    Remove '!' from create to test gracious error handling.  Add tests for missing attributes, non-matching password/pass confirm

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "User Registration" do
   end 
 
   it 'does not create a user if email isnt unique' do 
-    User.create!(name: 'User One', email: 'notunique@example.com', password: 'password123', password_confirmation: 'password123')
+    User.create(name: 'User One', email: 'notunique@example.com', password: 'password123', password_confirmation: 'password123')
 
     visit register_path
 
@@ -28,5 +28,64 @@ RSpec.describe "User Registration" do
 
     expect(current_path).to eq(register_path)
     expect(page).to have_content("Email has already been taken")
+  end
+
+  it 'if a user fails to enter a name, unique email, or matching passwords, a flash message will be displayed letting them know what went wrong' do
+    visit register_path
+
+    name = "Matt"
+    email = 'whatever@example.com'
+    password = 'password123'
+    password_confirmation = 'password123'
+
+    fill_in :user_name, with: ""
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: password_confirmation
+
+    click_button "Create New User"
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Name can't be blank")
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: ""
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: password_confirmation
+
+    click_button "Create New User"
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Email can't be blank")
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: ""
+    fill_in :user_password_confirmation, with: password_confirmation
+
+    click_button "Create New User"
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password can't be blank")
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: ""
+
+    click_button "Create New User"
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation can't be blank and Password confirmation doesn't match Password")
+
+    fill_in :user_name, with: name
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
+    fill_in :user_password_confirmation, with: "notamatchingpassword"
+
+    click_button "Create New User"
+
+    expect(current_path).to eq(register_path)
+    expect(page).to have_content("Password confirmation doesn't match Password")
   end
 end
