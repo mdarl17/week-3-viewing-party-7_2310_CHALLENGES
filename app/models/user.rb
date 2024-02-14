7de16e8c5e3a0372ab039d19be4a4a941bd8e6c2
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-14 00:50:12 -0700

    Add validations for :password, and :password_confirmation.  Had to finagle different validations to allow shoulda_matchers to finally pass.  VERY FINICKY!!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord 
-  validates_presence_of :email, :name, :password, :password_confirmation
-  validates_uniqueness_of :email, case_sensitive: false
+  validates_presence_of :name, allow_nil: false
+  validates_presence_of :email, allow_nil: false, case_sensitive: false
+  validates_uniqueness_of :email
+  validates_presence_of :password_digest, allow_nil: false
+  validates_presence_of :password_confirmation, allow_nil: false
+  validates_presence_of :password, allow_nil: false
   has_many :viewing_parties
   
   has_secure_password
