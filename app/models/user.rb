7542ce81d299b7f3c20dee794833fe8f86e63703
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-13 23:19:13 -0700

    Add validations for new password and password_confirmation columns

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
-class User <ApplicationRecord 
+class User < ApplicationRecord 
   validates_presence_of :email, :name, :password, :password_confirmation
-  validates_uniqueness_of :email
+  validates_uniqueness_of :email, case_sensitive: false
   has_many :viewing_parties
   
   has_secure_password
-end 
+end
