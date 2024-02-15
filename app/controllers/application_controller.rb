6c8726c15d81a3daf80c9f9e5cf6c8888974eee8
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-15 03:15:45 -0700

    Controller method to create cookie JSON objects to store info about users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def bake_cookies
+    user = current_user
+    cookies[user.email] = JSON.generate({
+        location: params[:location],
+        expires: 1.week.from_now
+      })
+  end
+
+  def current_user
+    @_current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  private
 end
