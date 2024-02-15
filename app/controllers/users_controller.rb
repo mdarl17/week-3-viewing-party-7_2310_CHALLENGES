2d15a72e82c3bff8213ace1921157425379276c3
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-15 03:17:03 -0700

    Add code to create JSON formatted cookies with user data that needs to persist after session ends

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,5 @@
+require 'uri'
+
 class UsersController <ApplicationController 
   def new 
     @user = User.new()
@@ -5,11 +7,16 @@ class UsersController <ApplicationController
 
   def show 
     @user = User.find(params[:id])
+    email = @user.email
+    @cookies = JSON.parse(cookies[@user.encode(email)], symbolize_names: true) if cookies[@user.encode(email)]
   end 
 
   def create 
     user = User.create(user_params)
+
     if user.save
+      session[:user_id] = user.id
+      @cookies = JSON.parse(bake_cookies, symbolize_names: true)
       redirect_to user_path(user)
     else  
       flash[:error] = user.errors.full_messages.to_sentence
@@ -22,7 +29,6 @@ class UsersController <ApplicationController
 
   def login
     user = User.find_by(email: params[:email])
-
     if !user
       flash[:error] = "Sorry, we could't find an account with the email you entered."
       render :login_form
@@ -30,6 +36,8 @@ class UsersController <ApplicationController
       flash[:error] = "Sorry, the password entered does not match the one we have on file. Please try logging in again."
       render :login_form
     else
+      session[:user_id] = user.id
+      @cookies = JSON.parse(bake_cookies, symbolize_names: true)
       flash[:message] = "Welcome back, #{user.name}!"
       redirect_to user_path(user.id)
     end
