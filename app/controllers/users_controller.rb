cc659e41fd09343ef9dbf537311f982fdf001f1f
Matt Darlington <matthewsdarlington@gmail.com>
2024-02-19 19:04:03 -0700

    Add email and cookies

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,16 +7,15 @@ class UsersController <ApplicationController
 
   def show 
     @user = User.find(params[:id])
-    email = @user.email
-    @cookies = JSON.parse(cookies[@user.encode(email)], symbolize_names: true) if cookies[@user.encode(email)]
+    @location = JSON.parse(cookies.signed[:form_data], symbolize_names: true)[:location] if cookies.signed[:form_data]
   end 
 
   def create 
     user = User.create(user_params)
+    session[:user_id] = user.id
 
     if user.save
-      session[:user_id] = user.id
-      @cookies = JSON.parse(bake_cookies, symbolize_names: true)
+      store_form_data(email: user.email, location: params[:location])
       redirect_to user_path(user)
     else  
       flash[:error] = user.errors.full_messages.to_sentence
@@ -25,10 +24,13 @@ class UsersController <ApplicationController
   end 
 
   def login_form
+    @location = JSON.parse(cookies.signed[:form_data], symbolize_names: true)[:location] if cookies.signed[:form_data]
+    @email = JSON.parse(cookies.signed[:form_data], symbolize_names: true)[:email] if cookies.signed[:form_data]
   end
 
   def login
     user = User.find_by(email: params[:email])
+    session[:user_id] = user.id
     if !user
       flash[:error] = "Sorry, we could't find an account with the email you entered."
       render :login_form
@@ -36,11 +38,16 @@ class UsersController <ApplicationController
       flash[:error] = "Sorry, the password entered does not match the one we have on file. Please try logging in again."
       render :login_form
     else
-      session[:user_id] = user.id
-      @cookies = JSON.parse(bake_cookies, symbolize_names: true)
+      store_form_data(email: user.email, location: params[:location])
       flash[:message] = "Welcome back, #{user.name}!"
       redirect_to user_path(user.id)
     end
+  end
+
+  def logout
+    reset_session
+
+    redirect_to root_path, notice: "You have been logged out."
   end
 
   private 
@@ -48,4 +55,14 @@ class UsersController <ApplicationController
   def user_params 
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
+
+  def store_form_data(data)
+    cookies.signed[:form_data] = {
+        value: {
+          email: data[:email],
+          location: data[:location]
+        }.to_json,
+      expires: 1.week.from_now
+    }
+  end
 end 
